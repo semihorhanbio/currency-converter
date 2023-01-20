704f5e8a2e216b9fef7ba5c37208c6cad8597da1
Semih <50242428+semihorhanbio@users.noreply.github.com>
2023-01-20 11:31:48 +0300

    Update main.py
    calculate conversion to dollar

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Meet a conicoin!")
+convert_amount = int(input())
+print(f'I have {convert_amount} conicoins.')
+print(f'{convert_amount} conicoins cost {convert_amount*100} dollars.')
+print('I am rich! Yippee!')

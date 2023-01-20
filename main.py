a68d329424cebcfcc6337a3ca37639409c1d889a
Semih <50242428+semihorhanbio@users.noreply.github.com>
2023-01-20 12:56:35 +0300

    add feature to convert different exchange rates

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-convert_amount = int(input("Please, enter the number of conicoins you have: > "))
-exchange_rate = float(input("Please, enter the exchange rate: > "))
-print(f'The total amount of dollars: {convert_amount * exchange_rate}')
+convert_amount = float(input())
+exchange_rates = {"RUB": 2.98,"ARS": 0.82, "HNL": 0.17, "AUD": 1.9622, "MAD": 0.208}
+for currency, rate in exchange_rates.items():
+    print(f'I will get {round(convert_amount * rate, 2)} {currency} from the sale of {convert_amount} conicoins.')
